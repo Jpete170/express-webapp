ef395a35456c06fdc64aac3e1913e230d0fe097d
Jpete170 <39613600+Jpete170@users.noreply.github.com>
2022-02-07 18:04:58 +1000

    minor changes to airbnb routes

--- routes/airbnb.js
+++ routes/airbnb.js
@@ -7,7 +7,7 @@ const objID = require('mongodb').ObjectId;
 //let _db; //database variable
 
 /* The base AirBnB Sample dataset page*/
-//Will be limited to 5 responses for now, will be updated to reflect a "Show amount on Page" function via Frontend req
+//This is the default landing page, that will be displayed 
 router.route('/index').get(function(req, res, next){
   let dbConn = dbo.getDB("sample_airbnb");
   dbConn.collection("listingsAndReviews").find({}).limit(10).toArray(function(err, result){
@@ -17,11 +17,13 @@ router.route('/index').get(function(req, res, next){
    // next()
   });
   
+  //Implement a route for a desired "Show amount of Listings" option, which is to be implemented at some point
+  
+
   /**Get a single airbnb listing record */
   router.get('/:id', function(req, res){
     let dbConn = dbo.getDB("sample_airbnb");
     const reqID = req.params.id;
-    //const o_id = objID(reqID)
     dbConn.collection("listingsAndReviews").find({
       _id: reqID,
     }).toArray(function(err, result){
@@ -31,20 +33,6 @@ router.route('/index').get(function(req, res, next){
     })
     
   })
-  //Return only the name of a place, will be used for a specific front end feature
-  router.get('/name', function(req, res){
-    let dbConn = dbo.getDB("sample_airbnb");
-    let query ={
-      _id: "1003530"
-    }
-    dbConn
-    .collection("listingsAndReviews")
-    .find(query)
-    .project({name: 1})
-    .toArray(function(err, result){
-      if (err) throw err;
-      res.json(result)
-    })
-  })
+
 
 module.exports = router
